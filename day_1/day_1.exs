eb5640b05951d3e628eceaee24be46b3485da8a4
Greg Rychlewski <grychle@Gregs-MacBook-Pro.local>
2021-12-01 08:01:42 -0500

    oops there is part 2

--- day_1/day_1.exs
+++ day_1/day_1.exs
@@ -1,5 +1,7 @@
 defmodule Day1 do
-  @moduledoc """
+  @moduledoc false
+
+  @doc """
   As the submarine drops below the surface of the ocean, it automatically performs a sonar sweep of the nearby sea floor. On a small screen, the sonar sweep report (your puzzle input) appears: each line is a measurement of the sea floor depth as the sweep looks further and further away from the submarine.
 
   For example, suppose you had the following report:
@@ -34,8 +36,7 @@ defmodule Day1 do
 
   How many measurements are larger than the previous measurement?
   """
-
-  def solution1(input_path) do
+  def part1(input_path) do
     input_path
     |> File.stream!()
     |> Enum.reduce({nil, 0}, fn curr, {prev, count} ->
@@ -70,6 +71,57 @@ defmodule Day1 do
       end
     end)
   end
+
+  @doc """
+  Considering every single measurement isn't as useful as you expected: there's just too much noise in the data.
+
+  Instead, consider sums of a three-measurement sliding window. Again considering the above example:
+
+  199  A
+  200  A B
+  208  A B C
+  210    B C D
+  200  E   C D
+  207  E F   D
+  240  E F G
+  269    F G H
+  260      G H
+  263        H
+  Start by comparing the first and second three-measurement windows. The measurements in the first window are marked A (199, 200, 208); their sum is 199 + 200 + 208 = 607. The second window is marked B (200, 208, 210); its sum is 618. The sum of measurements in the second window is larger than the sum of the first, so this first comparison increased.
+
+  Your goal now is to count the number of times the sum of measurements in this sliding window increases from the previous sum. So, compare A with B, then compare B with C, then C with D, and so on. Stop when there aren't enough measurements left to create a new three-measurement sum.
+
+  In the above example, the sum of each three-measurement window is as follows:
+
+  A: 607 (N/A - no previous sum)
+  B: 618 (increased)
+  C: 618 (no change)
+  D: 617 (decreased)
+  E: 647 (increased)
+  F: 716 (increased)
+  G: 769 (increased)
+  H: 792 (increased)
+  In this example, there are 5 sums that are larger than the previous sum.
+
+  Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
+  """
+  def part2(input_path, window_size) do
+    input_path
+    |> File.stream!()
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&String.to_integer/1)
+    |> Stream.chunk_every(window_size, 1, :discard)
+    |> Enum.reduce({nil, 0}, fn curr, {prev_sum, count} ->
+      curr_sum = Enum.sum(curr)
+
+      if curr_sum > prev_sum do
+        {curr_sum, count + 1}
+      else
+        {curr_sum, count}
+      end
+    end)
+  end
 end
 
-IO.inspect(Day1.solution1("day_1/input.txt"))
+IO.inspect(Day1.part1("day_1/input.txt"))
+IO.inspect(Day1.part2("day_1/input.txt", 3))
